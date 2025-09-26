8ade36bb8e199579d4cd439c95c69d2ed23d3c85
nic_bushuev <nic.funnyhouse@gmail.com>
2025-09-26 11:51:54 +0300

    add func widget

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,19 @@
-def get_mask_card_number(card_number: int) -> str:
+from typing import Union
+
+
+
+def get_mask_card_number(card_number: Union[str, int]) -> str:
     """Функция, которая маскирует введенный номер карты согласно заданной маске"""
+
+    if len(card_number) != 16:
+        raise "Неверный формат ввода! Должно быть 16 цифр"
+
 
     card_number = str(card_number)
     return "{} {}** **** {}".format(card_number[:4], card_number[4:6], card_number[12:])
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: Union[str,int]) -> str:
     """Функция, которая маскирует введенный номер карты согласно заданной маске"""
 
     account_number = str(account_number)
--- src/widget.py
+++ src/widget.py
@@ -1,24 +1,38 @@
 from src.masks import get_mask_card_number, get_mask_account
 
 
-def mask_account_card(payment_info:str)->str:
-	"""Функция, принимающая на вход информацию о платежном инструменте и применяющая соответствующую маску"""
+
+def mask_account_card(payment_info: str) -> str:
+	"""Функция маскирует номер карты или счета"""
 	payment_info = str(payment_info)
+	split_payment_info = payment_info.split()
 
-	split_payment_info = payment_info.lower().split(" ")
+	# print(split_payment_info)  # для отладки
 
+	if len(split_payment_info) < 2:
+		raise ValueError("Недостаточно данных")
 
-	if split_payment_info[0].startswith(("visa",'mastercard',"maestro","mir")):
-		masked_number = f'{split_payment_info[0]} + ' get_mask_card_number(split_payment_info[])
-	elif payment_info.lower().startswith(("cчет")):
-		masked_number = get_mask_account(payment_info)
+	first_word_lower = split_payment_info[0].lower()
+
+	# Обрабатываем карты
+	if first_word_lower.startswith(("visa", 'mastercard', "maestro", "mir")):
+		if len(split_payment_info) > 2:
+			# Многословное название (например, "Visa Platinum")
+			card_name = " ".join(split_payment_info[:-1])  # все кроме номера
+		else:
+			# Однословное название (например, "Maestro")
+			card_name = split_payment_info[0]
+
+		return f'{card_name} {get_mask_card_number(split_payment_info[-1])}'
+
+	# Обрабатываем счета
+	elif first_word_lower.startswith("счет"):
+		return f'Счет {get_mask_account(split_payment_info[-1])}'
+
 	else:
-		raise ValueError("Неверный формат ввода")
+		raise ValueError(f"Неизвестный тип: {payment_info}")
 
-	print(masked_number)
-
-mask_account_card("Maestro 1596837868705199")
-
-
-
+print(mask_account_card("Visa Platinum 1234123412341234"))
+print(mask_account_card("Счет 123123123123"))
+print(mask_account_card("Visa 1234123412341234"))
 
