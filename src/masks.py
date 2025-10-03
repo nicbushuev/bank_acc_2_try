f5d16d08e94febc2f7dc06baf74dac0e4229ac83
nic_bushuev <nic.funnyhouse@gmail.com>
2025-10-03 20:26:09 +0300

    update test_masks

--- src/masks.py
+++ src/masks.py
@@ -1,35 +1,46 @@
 from typing import Union
 
 
-
 def get_mask_card_number(card_number: Union[str, int]) -> str:
-    """Функция, которая маскирует введенный номер карты согласно заданной маске"""
+	"""Функция, которая маскирует введенный номер карты согласно заданной маске"""
 
-# Проверка на наличие пустой строки:
-    if card_number == "":
-        raise ValueError("Отсутствует номер карты")
+	# Проверка на наличие пустой строки:
+	if card_number == "":
+		raise ValueError("Отсутствует номер карты")
 
-# Проверка на наличие пробела:
-    if card_number.isspace():
-        raise ValueError("В номере карты не должно быть пробелов")
-#Проверка на наличие 16 цифр в номере карты:
-    if len(str(card_number)) != 16:
-        raise ValueError("Неверный формат ввода! Должно быть 16 цифр")
+	# Проверка на наличие пробела:
+	if card_number.isspace():
+		raise ValueError("В номере карты не должно быть пробелов")
+	# Проверка на наличие 16 цифр в номере карты:
+	if len(str(card_number)) != 16:
+		raise ValueError("Неверный формат ввода! Должно быть 16 цифр")
 
-#Проверка на наличие только цифр:
-    if not card_number.isdigit():
-        raise ValueError("Неверный формат ввода, не должно быть букв в номере карты")
+	# Проверка на наличие только цифр:
+	if not card_number.isdigit():
+		raise ValueError("Неверный формат ввода, не должно быть букв в номере карты")
 
-
-
-
-    card_number = str(card_number)
-    return "{} {}** **** {}".format(card_number[:4], card_number[4:6], card_number[12:])
+	card_number = str(card_number)
+	return "{} {}** **** {}".format(card_number[:4], card_number[4:6], card_number[12:])
 
 
-def get_mask_account(account_number: Union[str,int]) -> str:
-    """Функция, которая маскирует введенный номер карты согласно заданной маске"""
+def get_mask_account(account_number: Union[str, int]) -> str:
+	"""Функция, которая маскирует введенный номер карты согласно заданной маске"""
 
-    account_number = str(account_number)
-    return "**" + account_number[-4:]
+	# Проверка на наличие пустой строки:
+	if account_number == "":
+		raise ValueError("Отсутствует номер счета")
 
+	# Проверка на наличие пробела:
+	if account_number.isspace():
+		raise ValueError("В номере счета не должно быть пробелов")
+	# Проверка на наличие 20 цифр в номере счета:
+	if len(str(account_number)) != 20:
+		raise ValueError("Неверный формат ввода! Должно быть 20 цифр")
+
+	# Проверка на наличие только цифр:
+	if not account_number.isdigit():
+		raise ValueError("Неверный формат ввода, не должно быть букв в номере счета")
+
+
+	account_number = str(account_number)
+	return "**" + account_number[-4:]
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,6 +4,7 @@ import pytest
 
 """ТЕСТИРОВАНИЕ get_mask_card_number"""
 
+
 def test_get_mask_card_number(default_card_number):
 	"""Тест с валидным номером карты"""
 	assert get_mask_card_number(default_card_number) == "1234 12** **** 1234"
@@ -32,11 +33,14 @@ def test_get_mask_card_number_invalid(invalid_card_number, error_match):
 	with pytest.raises(ValueError, match=error_match):
 		get_mask_card_number(invalid_card_number)
 
+
 """ТЕСТИРОВАНИЕ get_mask_account"""
+
 
 def test_get_mask_account_number(default_account_number):
 	"""Тест с валидным номером счета"""
 	assert get_mask_account(default_account_number) == "**2345"
+
 
 @pytest.mark.parametrize("account_number, expected", [
 	("12345678901234567890", "**7890"),
@@ -44,6 +48,18 @@ def test_get_mask_account_number(default_account_number):
 	("11112222333344445555", "**5555"),
 	("00001111222233333211", "**3211"),
 ])
-
-def test_account_number_parametrize(account_number,expected):
+def test_account_number_parametrize(account_number, expected):
+	"""Тест с маскировки различных номеров счета с параметризацией"""
 	assert get_mask_account(account_number) == expected
+
+
+@pytest.mark.parametrize("invalid_account_number, error_match", [
+	("", "Отсутствует номер счета"),
+	(" ", "В номере счета не должно быть пробелов"),
+	("123", "Неверный формат ввода! Должно быть 20 цифр"),
+	("1234abcd123412341234", "Неверный формат ввода, не должно быть букв в номере счета"),
+])
+def test_get_mask_account_invalid(invalid_account_number, error_match):
+	"""Тест на отработку ошибок ValueError с параметризацией"""
+	with pytest.raises(ValueError, match=error_match):
+		get_mask_account(invalid_account_number)
